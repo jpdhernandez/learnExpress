ee358e79e55ff9106bf0d610a9559c3d5aee16d8
jpdhernandez <jpdhernandez@gmail.com>
2017-02-05 21:00:27 -0500

    Refactor and add more templates
     - register hbs helpers and partials

--- server.js
+++ server.js
@@ -2,22 +2,28 @@ const express = require("express");
 const app = express();
 const hbs = require("hbs");
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
-
 app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("allcaps", (text) => {
+    return text.toUpperCase();
+});
 
 app.get("/", (req, res) => {
     res.render("home.hbs", {
         pageTitle: "Home Page",
-        welcomeMessage: "Welcome to my website",
-        currentYear: new Date().getFullYear()
+        welcomeMessage: "Welcome to my website"
     });
 });
 
 app.get("/about", (req, res) => {
     res.render("about.hbs", {
-        pageTitle: "About Page",
-        currentYear: new Date().getFullYear()
+        pageTitle: "About Page"
     });
 })
 
